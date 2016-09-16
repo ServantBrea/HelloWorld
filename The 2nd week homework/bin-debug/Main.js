6f0bb96a9a74e8f6d0e0835e4e1c62ab067f22a2
ServantBrea <516916849@qq.com>
2016-09-16 21:35:00 +0800

    金字塔算法写好了，但是怎么调用啊！！！！！！！！！！！

--- The 2nd week homework/bin-debug/Main.js
+++ The 2nd week homework/bin-debug/Main.js
@@ -1,4 +1,24 @@
 console.log("Please give a number!");
-void Function(n, number);
-{
-}
+BuildPyramid(4);
+void function BuildPyramid(n) {
+    var i;
+    var j;
+    var k;
+    var x = " ";
+    var y = "*";
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - (i + 1); j++) {
+            console.log(x);
+        } //画前空格
+        for (k = 0; k < i + 1; k++) {
+            console.log(y);
+            if (k < i) {
+                console.log(x);
+            }
+        } //画*串
+        for (j = 0; j < n - (i + 1); j++) {
+            console.log(x);
+        } //画后空格
+        console.log("\n");
+    }
+};
--- The 2nd week homework/src/Main.ts
+++ The 2nd week homework/src/Main.ts
@@ -1,16 +1,30 @@
 console.log("Please give a number!");
 
-void Function(n:number) {
+BuildPyramid(4);
+
+void function BuildPyramid(n:number) {
     var i:number;
     var j:number;
-    var z:number;
+    var k:number;
     var x:string=" ";
     var y:string="*";
+    
     for(i=0;i<n;i++){
+        
         for(j=0;j<n-(i+1);j++){
             console.log(x);
         }//画前空格
-        for(o)
         
+        for(k=0;k<i+1;k++){
+            console.log(y);
+            if(k<i){
+                console.log(x);}
+        }//画*串
+        
+        for(j=0;j<n-(i+1);j++){
+            console.log(x);
+        }//画后空格
+            
+        console.log("\n");
     }
 }
